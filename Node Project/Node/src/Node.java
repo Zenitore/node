d2ff0a0b241217ccbdfc4a1192f69be87fc82790
Zenitore <eayers25@kentdenver.org>
2023-09-22 11:23:30 -0600

    idk if it commited the last time but this will be the addition of all methods and testing

--- Node Project/Node/src/Node.java
+++ Node Project/Node/src/Node.java
@@ -1,8 +1,29 @@
 public class Node <T>{
     private T t;
+    private Node parentNode;
+    private Node childNode;
+
+
+    public Node(){
+        t = null;
+        parentNode = null;
+        childNode = null;
+    }
+    public Node(T t, Node parentNode, Node childNode){
+        this.t = t;
+        this.parentNode = parentNode;
+        this.childNode = childNode;
+    }
+    public Node(T t){
+        this.t = t;
+    }
 
     public void set(T t) { this.t = t; }
     public T get() { return t; }
+    public String toString() {return  "Node: " + get();}
+    public boolean equals(Node <T> p){ return get() == p.get();}
+
 
     }
+
         
--- Node Project/Node/src/TestNode.java
+++ Node Project/Node/src/TestNode.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class TestNode {
+
+
+public class TestNode {
+    public static void main(String[] args) {
+        Node<Integer> thing1 = new Node<>(4);
+        Node<Integer> thing2 = new Node<>(4);
+        Node<Integer> thing3 = new Node<>(3,thing1,thing2);
+        System.out.println(thing1.toString());
+        System.out.println(thing1.equals(thing2));
+        System.out.println(thing3);
+    }
 }
