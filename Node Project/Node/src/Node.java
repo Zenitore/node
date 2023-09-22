02875829015cfaab4e8af4add82875e122617f48
Zenitore <eayers25@kentdenver.org>
2023-09-22 11:48:56 -0600

    added comments and the rest of the methods to both set and get the parent and child Nodes

--- Node Project/Node/src/Node.java
+++ Node Project/Node/src/Node.java
@@ -1,27 +1,45 @@
+/*
+ETLA
+9/22/23
+LDO
+google + the api ms hellman gave us
+*/
+
+
+
+
 public class Node <T>{
-    private T t;
-    private Node parentNode;
-    private Node childNode;
+    private T t; // new type T variable t. T is defined when the object is created
+    private Node parentNode; //new Node object that is the parent of this created node
+    private Node childNode; //new Node object that is the child of this created node
 
 
     public Node(){
-        t = null;
+        t = null;//default sets all values to null. Values can be saved in later
         parentNode = null;
         childNode = null;
     }
     public Node(T t, Node parentNode, Node childNode){
-        this.t = t;
+        this.t = t;//three inputs allowing for all instance values to be set
         this.parentNode = parentNode;
         this.childNode = childNode;
     }
     public Node(T t){
         this.t = t;
-    }
+    } // one input for allowance of the instance variable t to be set
 
-    public void set(T t) { this.t = t; }
-    public T get() { return t; }
-    public String toString() {return  "Node: " + get();}
-    public boolean equals(Node <T> p){ return get() == p.get();}
+    public void setNodeValue(T t) { this.t = t; }//sets the value inside the node object
+    public T getNodeValue() { return t; }//returns the value inside the obj node
+    public void setParentNode (Node parentNode){
+        this.parentNode=parentNode;
+    }//sets the parent node of the node obj
+    public Node getParentNode(){return this.parentNode;}//returns the parent node of the node obj
+    public void setChildNode(Node childNode){this.childNode=childNode;}//sets the child node of the node obj
+    public Node getChildNode(){
+        return this.childNode;
+    }//returns the child node of the node obj
+    public String toString() {return  "Node: " + getNodeValue();}//returns the String of Node then the value that has been saved in the node
+    public boolean equals(Node <T> p){ return getNodeValue() == p.getNodeValue();}//checks to see if 2 values of the nodes are equivalent
 
 
     }
--- Node Project/Node/src/TestNode.java
+++ Node Project/Node/src/TestNode.java
@@ -2,11 +2,11 @@
 
 public class TestNode {
     public static void main(String[] args) {
-        Node<Integer> thing1 = new Node<>(4);
-        Node<Integer> thing2 = new Node<>(4);
-        Node<Integer> thing3 = new Node<>(3,thing1,thing2);
-        System.out.println(thing1.toString());
-        System.out.println(thing1.equals(thing2));
-        System.out.println(thing3);
+        Node<Integer> thing1 = new Node<>(4);//creates new node obj called thing1 with value 4
+        Node<Integer> thing2 = new Node<>(4);//creates new node obj called thing2 with value 4
+        Node<Integer> thing3 = new Node<>(3,thing1,thing2);//creates new node obj called thing3 with value 3 & a parent of thing 1 and child of thing 2
+        System.out.println(thing1.toString());//prints the value stored in thing 1
+        System.out.println(thing1.equals(thing2));//prints the equivalancy statement betweeen thing 1 and thing 2
+        System.out.println(thing3);//prints thing 3
     }
 }
